17ec9f4cc970d737ba0ef0eb77dccc7982daa00f
Vikashlab <vikash7618@gmail.com>
2024-05-26 05:05:37 +0530

    upload file

---
+++ 76. Write a program to check if a string contains a specific sub-string.py
@@ -0,0 +1,8 @@
+main_string="this is my world"
+
+sub_string="my"
+
+if sub_string in main_string:
+    print(sub_string,"is present",main_string)
+else:
+    print(sub_string,"is not present",main_string)    
