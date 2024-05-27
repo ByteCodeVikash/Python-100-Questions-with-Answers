cec2ecf273564c2db8cd6faf99dd133a9b0708e2
Vikashlab <vikash7618@gmail.com>
2024-05-28 05:06:12 +0530

    upload file

---
+++ 87. Write a program to perform string formatting using placeholders.py
@@ -0,0 +1,18 @@
+#using f string method
+a="Vikash"
+b="Dubey"
+
+print(f"I am {a} {b}")
+
+#using format method
+
+a="Shivam"
+b="Pandey"
+
+print("I am {} {}".format(a,b))
+
+#using '%' method
+a="Jalaj"
+b="Mishra"
+
+print("I am %s %s" %(a,b))
