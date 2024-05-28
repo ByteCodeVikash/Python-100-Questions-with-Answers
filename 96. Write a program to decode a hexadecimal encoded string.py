fea9d74c7fa0a547d4cdf434d686ed56528d54e3
Vikashlab <vikash7618@gmail.com>
2024-05-29 05:03:23 +0530

    upload file

---
+++ 96. Write a program to decode a hexadecimal encoded string.py
@@ -0,0 +1,12 @@
+def hex_to_string(hex_string):
+    byte_String=bytes.fromhex(hex_string)
+
+    original_string=byte_String.decode('utf-8')
+
+    return original_string
+
+hex_encoded_string="48656c6c6f20776f726c64"
+decoded_string=hex_to_string(hex_encoded_string)
+
+print("Hexadecimal encoded string",hex_encoded_string)
+print("Decoded original string:",decoded_string)
